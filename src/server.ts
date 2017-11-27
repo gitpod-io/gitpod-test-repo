2df627fd227c68626628fccecb9e4633e2ac7d04
somefox <dummy@example.org>
2017-11-27 21:53:52 +0000

    Renamed servers -> server and added responses

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.use(function (req: any, res: any, next: any) {
+    res.setHeader('X-Powered-By', 'Rainbows');
+    console.log("x-port: " + req.headers['x-port']);
+    console.log("x-path: " + req.headers['x-path']);
+    next();
+})
+
+app.get("/", (req: any, res: any) => {
+    let result = "Received a GET request on /";
+    console.log(result);
+    res.status(200).send(result);
+});
+app.get("/test", (req: any, res: any) => {
+    let result = "Received a GET request on /test";
+    console.log(result);
+    res.status(200).send(result);
+});
+
+const httpServer = app.listen(3000, () => {
+    console.log('Server listening on port %s.', httpServer.address().port);
+});
